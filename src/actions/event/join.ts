9350adaa843ffa22f730657224e703695ac463a8
Geno Ferollo <sakhib.orzklv@icloud.com>
2021-05-27 17:06:44 +0500

    Update join.ts

--- src/actions/event/join.ts
+++ src/actions/event/join.ts
@@ -6,10 +6,10 @@ import env from '@core/env'
 composer.on('new_chat_members', async (ctx: TelegrafContext) => {
     if (ctx.chat.type !== 'private') {
         if (
-            ctx.chat.id !== parseInt(env.GROUP) // ||
-            // ctx.chat.id !== -1001456005278
+            ctx.chat.id !== parseInt(env.GROUP) ||
+            ctx.chat.id !== -1001456005278
         ) {
-            await ctx.replyWithHTML(`Men bu guruh boshqarishdan bosh tortaman!`)
+            await ctx.replyWithHTML(`<b>Men bu guruh boshqarishdan bosh tortaman, bu Xinux jamiyatiga tegishli guruh emas!</b>`)
             await ctx.telegram.leaveChat(ctx.chat.id)
         }
     }
