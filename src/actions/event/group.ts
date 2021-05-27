b9f64792d3745f634d56378e05f79df640af0d99
Geno Ferollo <sakhib.orzklv@icloud.com>
2021-05-27 17:00:30 +0500

    Update group.ts

--- src/actions/event/group.ts
+++ src/actions/event/group.ts
@@ -5,10 +5,22 @@ import * as keyboard from '@layouts/keyboards'
 import { TelegrafContext } from '@type/telegraf'
 
 composer.on('new_chat_members', async (ctx: TelegrafContext) => {
-    await ctx.telegram.sendMessage(ctx.chat.id, message.newMember(ctx), {
-        parse_mode: 'HTML',
-        reply_markup: keyboard.newMember
-    })
+    if (ctx.message.new_chat_members.some((user) => user.username === ctx.me)) {
+        await ctx.telegram.sendMessage(
+            ctx.chat.id,
+            `<b>Bugundan sizlarga guruhni boshqarishda yordam beraman!</b>`,
+            {
+                parse_mode: 'HTML'
+            }
+        )
+    }
+
+    if (ctx.message.new_chat_members.some((user) => user.username !== ctx.me)) {
+        await ctx.telegram.sendMessage(ctx.chat.id, message.newMember(ctx), {
+            parse_mode: 'HTML',
+            reply_markup: keyboard.newMember
+        })
+    }
 })
 
 middleware(composer)
