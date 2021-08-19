494b2ac33e6bb53ae51c3c6ea0a473156d85fb18
Geno Ferollo <sakhib.orzklv@icloud.com>
2021-08-19 16:37:21 +0500

    Update ds.ts

--- src/database/ds.ts
+++ src/database/ds.ts
@@ -5,7 +5,8 @@
  * @return JSON | null
  */
 import axios, { AxiosResponse } from 'axios'
-
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// eslint-disable-next-line  @typescript-eslint/no-explicit-any
 export default async (link: string | URL): Promise<any> => {
     try {
         const response: AxiosResponse = await axios.get(<string>link)
